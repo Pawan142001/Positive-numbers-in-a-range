980c5046459da0a78baf72a54462e7367cd04c4d
Pawan142001 <90554117+Pawan142001@users.noreply.github.com>
2021-09-15 13:28:04 +0530

    Command to print

---
+++ positivenum.py
@@ -0,0 +1,12 @@
+x = int(input("Enter the start of range : "))
+
+m = int(input("Enter the end of range :"))
+
+#iterating each number in list
+
+for num in range(start , end + 1 ):
+
+#checking condition
+    if num >= 0:
+        print(num,end = "  " )
+    
